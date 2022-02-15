b8d638e151f6781b14ada8ac5f1ea5996af304cc
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2022-02-15 16:41:26 -0500

    Add files via upload

---
+++ SMFRC/src/main/java/frc/robot/commands/Eject.java
@@ -0,0 +1,49 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class Eject extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+  IntakeSubsystem intake;
+  ShooterSubsystem shooter;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public Eject(IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
+    
+    intake = intakeSubsystem;
+    shooter = shooterSubsystem;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake, shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intake.startEject();;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopEject();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
