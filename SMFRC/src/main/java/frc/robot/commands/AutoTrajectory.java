ed61611639ff3c1bb6b7bd51f7075d8d59547e0e
zzpdavid2 <zzpdavid2L@gmail.com>
2022-02-17 10:56:01 -0500

    fixed trajectory

--- SMFRC/src/main/java/frc/robot/commands/AutoTrajectory.java
+++ SMFRC/src/main/java/frc/robot/commands/AutoTrajectory.java
@@ -9,32 +9,24 @@ import frc.robot.subsystems.DriveSubsystem;
 
 import java.util.List;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 /** An example command that uses an example subsystem. */
-public class AutoTrajectory extends CommandBase {
+public class AutoTrajectory extends RamseteCommand {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  
-  private final DriveSubsystem drive;
-
-  private Trajectory trajectory;
-
-  private double startTime;
 
 // Create a voltage constraint to ensure we don't accelerate too fast
-DifferentialDriveVoltageConstraint autoVoltageConstraint =
+private DifferentialDriveVoltageConstraint autoVoltageConstraint =
     new DifferentialDriveVoltageConstraint(
         new SimpleMotorFeedforward(
             Constants.Drive_ks,
@@ -44,7 +36,7 @@ DifferentialDriveVoltageConstraint autoVoltageConstraint =
         10);
 
 // Create config for trajectory
-TrajectoryConfig config =
+private TrajectoryConfig config =
     new TrajectoryConfig(
             Constants.kMaxSpeed_Drive,
             Constants.kMaxAcceleration_Drive)
@@ -53,7 +45,19 @@ TrajectoryConfig config =
         // Apply the voltage constraint
         .addConstraint(autoVoltageConstraint);
 
-RamseteController controller = new RamseteController();
+private Trajectory trajectory = 
+TrajectoryGenerator.generateTrajectory(
+    // Start at the origin facing the +X direction
+    new Pose2d(0, 0, new Rotation2d(0)),
+    // Pass through these two interior waypoints, making an 's' curve path
+    List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+    // End 3 meters straight ahead of where we started, facing forward
+    new Pose2d(3, 0, new Rotation2d(0)),
+    // Pass config
+    config);
+    
+
+RamseteController controller;
 
   /**
    * Creates a new ExampleCommand.
@@ -64,61 +68,20 @@ RamseteController controller = new RamseteController();
    */
 
   public AutoTrajectory(Trajectory trajectory, DriveSubsystem driveSubsystem) {
-
-    drive = driveSubsystem;
-    this.trajectory = trajectory;
-
-    this.trajectory = 
-    TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        // Pass config
-        config);
-
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drive);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    startTime = Timer.getFPGATimestamp();
-    drive.resetEncoders();
-    drive.resetGyro();
-    drive.resetOdometry(new Pose2d());
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    double currTime = Timer.getFPGATimestamp() - startTime;
-
-    Trajectory.State goal = trajectory.sample(currTime);
-    ChassisSpeeds adjustedSpeeds = controller.calculate(drive.getPose(), goal);
-
-    DifferentialDriveWheelSpeeds wheelSpeeds = Constants.kDriveKinematics.toWheelSpeeds(adjustedSpeeds);
-    double left = wheelSpeeds.leftMetersPerSecond;
-    double right = wheelSpeeds.rightMetersPerSecond;
-
-    System.out.println("left: " + left);
-    System.out.println("right: " + right);
-    System.out.println(drive.getPose());
-    System.out.println(goal);
-
-    drive.setVoltage(left, right);
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+    super(
+      trajectory,
+      driveSubsystem::getPose,
+      new RamseteController(2.0, 0.7),
+      new SimpleMotorFeedforward(Constants.Drive_ks,
+                                Constants.Drive_kv,
+                                Constants.Drive_ka),
+      Constants.kDriveKinematics,
+      driveSubsystem::getWheelSpeeds,
+      new PIDController(Constants.DRIVETRAIN_VELOCITY_GAINS.kP, 0, 0),
+      new PIDController(Constants.DRIVETRAIN_VELOCITY_GAINS.kP, 0, 0),
+      // RamseteCommand passes volts to the callback
+      driveSubsystem::setVoltage,
+      driveSubsystem
+    );
   }
 }
